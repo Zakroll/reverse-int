1356dc00d272d5834d92bdff65dcfbe9ca7a08db
Petr Filippov <xakdnn@gmail.com>
2023-01-03 19:47:03 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    const string = String(Math.abs(n));
+    let result = '';
+    for(let i = string.length - 1; i >= 0; i -= 1) {
+      result += string[i];
+    }
+    return +result
 }
+
+
+
